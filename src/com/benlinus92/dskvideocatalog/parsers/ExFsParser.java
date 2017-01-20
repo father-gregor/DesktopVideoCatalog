71447d09157f20eb999c50df8984cb5236676e87
father-gregor <benlinus92@gmail.com>
2017-01-20 02:40:16 +0200

    Add new parser class

--- src/com/benlinus92/dskvideocatalog/parsers/ExFsParser.java
+++ src/com/benlinus92/dskvideocatalog/parsers/ExFsParser.java
@@ -3,19 +3,12 @@ package com.benlinus92.dskvideocatalog.parsers;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.nio.charset.Charset;
-import java.nio.charset.CharsetDecoder;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 
-import org.apache.http.Header;
-import org.apache.http.HeaderElement;
-import org.apache.http.HttpRequest;
 import org.apache.http.HttpResponse;
-import org.apache.http.ParseException;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
@@ -29,16 +22,16 @@ import com.benlinus92.dskvideocatalog.AppConstants;
 import com.benlinus92.dskvideocatalog.model.BrowserVideoItem;
 import com.benlinus92.dskvideocatalog.model.SimpleVideoItem;
 
-public class TreeTvParser implements Parser {
-	private final static String TREE_TV_FILMS_URL = "http://tree.tv/films/sortType/new/page/";
-	private final static String TREE_TV_SERIES_URL = "http://tree.tv/serials/sortType/new/page/";
-	private final static String TREE_TV_CARTOONS = "http://tree.tv/multfilms/sortType/new/page/";
-	private final static String TREE_TV_BASIC_URL = "http://tree.tv";
-	private final static DateTimeFormatter DATE_FORMAT = DateTimeFormatter.ofPattern("yyyy-MM-dd");
-	private final static String ID_SAMPLE = "TREE_TV_";
-	
+public class ExFsParser implements Parser {
+	private final static String EXFS_FILMS_URL = "http://ex-fs.net/films/page/";
+	private final static String EXFS_SERIES_URL = "http://ex-fs.net/series/page/";
+	private final static String EXFS_CARTOONS_URL = "http://ex-fs.net/cartoon/page/";
+	private final static String EXFS_BASIC_URL = "http://ex-fs.net";
+	private final static DateTimeFormatter DATE_FORMAT = DateTimeFormatter.ofPattern("d.MM.yyyy");
+	private final static String ID_SAMPLE = "EX_FS_";
+
 	@Override
-	public String getHtmlContent(String url) throws IOException, ClientProtocolException {
+	public String getHtmlContent(String url) throws IOException {
 		HttpClient client = HttpClientBuilder.create().build();
 		HttpGet request = new HttpGet(url);
 		request.addHeader("User-Agent", AppConstants.USER_AGENT); 
@@ -51,21 +44,21 @@ public class TreeTvParser implements Parser {
 			sb.append(line);
 		return sb.toString();
 	}
-	
+
 	@Override
 	public List<SimpleVideoItem> getVideoItemsByCategory(int category, int page) {
 		List<SimpleVideoItem> itemsList = new ArrayList<>();
 		try {
-			String url = TREE_TV_FILMS_URL;
+			String url = EXFS_FILMS_URL;
 			if(category == AppConstants.CATEGORY_FILMS)
-				url = TREE_TV_FILMS_URL;
+				url = EXFS_FILMS_URL;
 			else if(category == AppConstants.CATEGORY_SERIES)
-				url = TREE_TV_SERIES_URL;
+				url = EXFS_SERIES_URL;
 			else if(category == AppConstants.CATEGORY_CARTOONS)
-				url = TREE_TV_CARTOONS;
+				url = EXFS_CARTOONS_URL;
 			String html = getHtmlContent(url + Integer.toString(page));
 			Document content = Jsoup.parse(html);
-			for(Element elem: content.select("div.item")) {
+			for(Element elem: content.select("div.MiniPostAllForm")) {
 				itemsList.add(createCatalogVideoItemFromHtml(elem));
 			}
 		} catch(ClientProtocolException e) {
@@ -78,27 +71,28 @@ public class TreeTvParser implements Parser {
 	@Override
 	public SimpleVideoItem createCatalogVideoItemFromHtml(Element el) {
 		SimpleVideoItem item = new SimpleVideoItem();
-		item.setTitle(el.select("h2").first().text());
-		item.setUrl(TREE_TV_BASIC_URL + el.select("h2").first().select("a").attr("href"));
-		for(Element elem: el.select("img")) {
-			if(elem.attr("title").length() > 0) {
-				item.setPrevImg(elem.attr("src"));
-				break;
-			}
-		}
-		item.setYear(el.select("div.smoll_year").text());
-		item.setAddedDate(LocalDate.parse(el.select("div.date_create span").text(), DATE_FORMAT));
+		item.setTitle(el.select("div.MiniPostName").text());
+		item.setUrl(el.select("div.MiniPostName a").attr("href"));
+		item.setPrevImg(EXFS_BASIC_URL + el.select("a.MiniPostPoster img").attr("src"));
+		item.setYear(el.select("div.MiniPostInfo a").attr("title"));
+		String date = el.select("div.customInfo span").text().trim();
+		if(date.contains("Сегодня"))
+			item.setAddedDate(LocalDate.now());
+		else if(date.contains("Вчера"))
+			item.setAddedDate(LocalDate.now().minusDays(1));
+		else
+			item.setAddedDate(LocalDate.parse(date, DATE_FORMAT));
 		item.setId(ID_SAMPLE);
 		return item;
 	}
-	
+
 	@Override
 	public BrowserVideoItem getVideoItemByUrl(String url) {
 		BrowserVideoItem item = null;
 		try {
 			String html = getHtmlContent(url);
 			Document content = Jsoup.parse(html);
-			item = createBrowserVideoItemFromHtml(content.select("div.item").first());
+			item = createBrowserVideoItemFromHtml(content.select("div#dle-content").first());
 		} catch(ClientProtocolException e) {
 			e.printStackTrace();
 		} catch(IOException e) {
@@ -109,22 +103,27 @@ public class TreeTvParser implements Parser {
 	@Override
 	public BrowserVideoItem createBrowserVideoItemFromHtml(Element el) {
 		BrowserVideoItem item = new BrowserVideoItem();
-		item.setTitle(el.select("h1#film_object_name").text());
-		item.setPrevImg(el.select("img#preview_img").attr("src"));
+		item.setTitle(el.select("h1.view-caption").text());
+		item.setPrevImg(EXFS_BASIC_URL + el.select("div.FullstoryFormLeft img").attr("src"));
+		Elements infoElem = el.select("div.FullstoryInfo p");
+		item.setYear(infoElem.get(0).text());
+		item.setCountry(infoElem.get(1).text());
 		List<String> list = new ArrayList<>();
-		for(Element elem: el.select("a[data-rel='janrs[]']"))
-			list.add(elem.text());
+		for(Element elem: infoElem.get(2).select("a"))
+			list.add(elem.attr("title"));
 		item.setGenre(list);
-		item.setYear(el.select("div.list_year a").text());
-		item.setCountry(el.select("span.item").text());
-		item.setDirector(el.select("span.regiser_item").text());
-		item.setTranslation(el.select("div.section_item:contains(Перевод:)").select("span:not(.main)").text());
-		item.setDuration(el.select("div.section_item:contains(Длительность:)").select("span:not(.main)").text());
+		item.setTranslation(infoElem.get(3).text());
+		item.setDuration(infoElem.get(5).text());
+		item.setPlot(el.select("div.FullstorySubFormText").text());
 		list = new ArrayList<>();
-		for(Element elem: el.select("div.actors_content"))
-			list.add(elem.text());
+		for(Element elem: el.select("div.FullstoryKadrFormImgAc img"))
+			list.add(elem.attr("title"));
 		item.setCast(list);
-		item.setPlot(el.select("div.description").text());
+		for(Element elem: el.select("div.TabDopInfoBlockOne"))
+			if(elem.html().contains("Режиссер")) {
+				item.setDirector(elem.select("a").text());
+				break;
+			}
 		return item;
 	}
 
