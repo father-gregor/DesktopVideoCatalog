dd7d2fbdeb561d9daf261cc91623c5a64b015fc1
father-gregor <benlinus92@gmail.com>
2017-02-03 23:18:15 +0200

    New controller for popup menu

---
+++ src/com/benlinus92/dskvideocatalog/viewcontroller/ChooseMediaMenuController.java
@@ -0,0 +1,56 @@
+package com.benlinus92.dskvideocatalog.viewcontroller;
+
+import com.benlinus92.dskvideocatalog.PropertiesHandler;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.input.MouseButton;
+
+public class ChooseMediaMenuController {
+
+	private VideoListController mainCaller;
+	@FXML
+	private Label internalPlayerLabel;
+	@FXML
+	private Label webPlayerLabel;
+	@FXML
+	private Label userDefaultPlayerLabel;
+	@FXML
+	private Label copyLinkLabel;
+	@FXML
+	public void initialize() {
+		internalPlayerLabel.setText(PropertiesHandler.getInstance().getInternalPlayerLabel());
+		webPlayerLabel.setText(PropertiesHandler.getInstance().getWebPlayerLabel());
+		userDefaultPlayerLabel.setText(PropertiesHandler.getInstance().getDefaultPlayerLabel());
+		copyLinkLabel.setText(PropertiesHandler.getInstance().getCopyLinkLabel());
+	}
+	public void initializeMenuItems() {
+		internalPlayerLabel.setOnMouseClicked(me -> {
+			if(me.getButton().toString() == MouseButton.PRIMARY.toString()) {
+				System.out.println("INTERNAL PLAYER");
+				mainCaller.openInternalVideoStreamPlayer();
+			}
+		});
+		webPlayerLabel.setOnMouseClicked(me -> {
+			if(me.getButton().toString() == MouseButton.PRIMARY.toString()) {
+				System.out.println("WEB PLAYER");
+				
+			}
+		});
+		userDefaultPlayerLabel.setOnMouseClicked(me -> {
+			if(me.getButton().toString() == MouseButton.PRIMARY.toString()) {
+				System.out.println("DEFAULT PLAYER");
+				mainCaller.openUserDefaultPlayer();
+			}
+		});
+		copyLinkLabel.setOnMouseClicked(me -> {
+			if(me.getButton().toString() == MouseButton.PRIMARY.toString()) {
+				System.out.println("COPY LINK");
+				mainCaller.copyLinkToClipBoard();
+			}
+		});
+	}
+	public void setMainCaller(VideoListController mainCaller) {
+		this.mainCaller = mainCaller;
+	}
+}
