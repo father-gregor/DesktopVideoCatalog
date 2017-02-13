d866cb20c4ed1125c8f231c7349c5adb07dd53da
father-gregor <benlinus92@gmail.com>
2017-02-14 01:19:31 +0200

    Add code for parsing direct link to videofile

--- src/com/benlinus92/dskvideocatalog/parsers/ExFsParser.java
+++ src/com/benlinus92/dskvideocatalog/parsers/ExFsParser.java
@@ -2,7 +2,9 @@ package com.benlinus92.dskvideocatalog.parsers;
 
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.nio.charset.Charset;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
@@ -11,15 +13,21 @@ import java.util.Arrays;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import org.apache.http.Header;
+import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.config.RequestConfig;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.message.BasicHeader;
+import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.util.EntityUtils;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
@@ -33,6 +41,8 @@ import com.benlinus92.dskvideocatalog.model.MediaStream;
 import com.benlinus92.dskvideocatalog.model.SimpleVideoItem;
 import com.benlinus92.dskvideocatalog.model.VideoLink;
 import com.benlinus92.dskvideocatalog.model.VideoTranslationType;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 
 public class ExFsParser implements Parser {
 	private final static String EXFS_FILMS_URL = "http://ex-fs.net/films/page/";
@@ -49,7 +59,6 @@ public class ExFsParser implements Parser {
 		mediaStreamsList.add(MediaStream.HLS);
 		parserCategoryMap = new LinkedHashMap<>();
 		parserCategoryMap.put(PropertiesHandler.getInstance().getUnitFilmsName(), AppConstants.CATEGORY_FILMS);
-		parserCategoryMap.put(PropertiesHandler.getInstance().getUnitSeriesName(), AppConstants.CATEGORY_SERIES);
 		parserCategoryMap.put(PropertiesHandler.getInstance().getUnitCartoonsName(), AppConstants.CATEGORY_CARTOONS);
 	}
 
@@ -61,13 +70,13 @@ public class ExFsParser implements Parser {
 				.setConnectionRequestTimeout(1000 * timeout).build();
 		HttpClient client = HttpClientBuilder.create().setDefaultRequestConfig(config).build();
 		HttpGet request = new HttpGet(url);
-		request.addHeader("User-Agent", AppConstants.USER_AGENT); 
+		request.addHeader("User-Agent", AppConstants.MOBILE_USER_AGENT); 
 		HttpResponse response = client.execute(request);
 		System.out.println(EXFS_BASIC_URL + " -  status " + response.getStatusLine().getStatusCode());
 		for(Header header: Arrays.asList(response.getHeaders("Set-Cookie"))) {
-			if(header.getValue().contains("__cfduid=")) {
+			if(header.getValue().contains("__cfduid=") && !cookieStr.contains("__cfduid=")) {
 				cookieStr = cookieStr + header.getValue().split(";")[0] + "; ";
-			} else if(header.getValue().contains("PHPSESSID=")) {
+			} else if(header.getValue().contains("PHPSESSID=") && !cookieStr.contains("PHPSESSID=")) {
 				cookieStr = cookieStr + header.getValue().split(";")[0] + "; ";
 			}
 		}
@@ -127,6 +136,10 @@ public class ExFsParser implements Parser {
 			String html = getHtmlContent(url);
 			Document content = Jsoup.parse(html);
 			item = createBrowserVideoItemFromHtml(content.select("div#dle-content").first());
+			for(Element elem: content.select("div#rightholder iframe")) {
+				if(!elem.attr("src").isEmpty() && !elem.attr("src").contains("serial") && item != null)
+					item.setVideoTransTypeList(getVideoTranslationList(url, elem.attr("src"))); //videolist can only be retrieved by different link
+			}
 		} catch(ClientProtocolException e) {
 			e.printStackTrace();
 		} catch(IOException e) {
@@ -158,31 +171,76 @@ public class ExFsParser implements Parser {
 				item.setDirector(elem.select("a").text());
 				break;
 			}
-		item.setVideoTransTypeList(getVideoTranslationList(el.select("div#rightholder iframe").attr("src")));
 		return item;
 	}
 	
-	private List<VideoTranslationType> getVideoTranslationList(String iframeLink) {
+	private List<VideoTranslationType> getVideoTranslationList(String originalUrl, String iframeLink) {
 		List<VideoTranslationType> videoList = new ArrayList<>();
 		System.out.println(iframeLink);
 		try {
 			Header userAgent = new BasicHeader("User-Agent", AppConstants.MOBILE_USER_AGENT);
 			Header cookie = new BasicHeader("Cookie", cookieStr);
+			Header referer = new BasicHeader("Referer", originalUrl);
 			int timeout = 6;
 			RequestConfig config = RequestConfig.custom()
 					.setConnectTimeout(1000 * timeout)
 					.setConnectionRequestTimeout(1000 * timeout).build();
 			HttpClient client = HttpClientBuilder.create().setDefaultRequestConfig(config).build();
-			HttpGet request = new HttpGet(iframeLink);
-			request.addHeader(userAgent);
-			request.addHeader(cookie);
-			HttpResponse response = client.execute(request);
+			HttpGet getRequest = new HttpGet(iframeLink);
+			getRequest.addHeader(userAgent);
+			getRequest.addHeader(cookie);
+			getRequest.addHeader(referer);
+			HttpResponse response = client.execute(getRequest);
+			for(Header header: Arrays.asList(response.getHeaders("Set-Cookie"))) {
+				if(header.getValue().contains("_moon_session=") && !cookieStr.contains("_moon_session=")) {
+					cookieStr = cookieStr + header.getValue().split(";")[0] + "; ";
+					break;
+				}
+			}
+			cookie = new BasicHeader("Cookie", cookieStr);
+			referer = new BasicHeader("Referer", iframeLink);
 			String webcontent = EntityUtils.toString(response.getEntity(), Charset.forName("UTF-8"));
-			System.out.println(webcontent);
-		} catch(IOException e) {
+			String videolistUrl = parseStringByTemplate("(http:\\/\\/[a-zA-Z0-9\\W]+?)\\/", iframeLink) + "/sessions/new_session";
+			System.out.println("URL - " + videolistUrl);
+			HttpPost postRequest = new HttpPost(videolistUrl);
+			postRequest.setEntity(new UrlEncodedFormEntity(getFormDataForPostRequest(webcontent)));
+			postRequest.addHeader(userAgent);
+			postRequest.addHeader(cookie);
+			postRequest.addHeader(referer);
+			postRequest.addHeader("X-Condition-Safe", "Normal");
+			postRequest.addHeader("X-Requested-With", "XMLHttpRequest");
+			response = client.execute(postRequest);
+			webcontent = EntityUtils.toString(response.getEntity(), Charset.forName("UTF-8"));
+			JsonParser jsonP = new JsonParser();
+			JsonObject jsonObj = jsonP.parse(webcontent).getAsJsonObject();
+			//videoList.add(new VideoTranslationType().se)
+			System.out.println(jsonObj.getAsJsonObject("mans").getAsJsonPrimitive("manifest_m3u8").getAsString());
+			
+		} catch(IOException | UnsupportedOperationException e) {
 			e.printStackTrace();
 		}
 		return videoList;
+	}
+	private List<BasicNameValuePair> getFormDataForPostRequest(String webcontent) {
+		List<BasicNameValuePair> list = new ArrayList<>();
+		list.add(new BasicNameValuePair("video_token", parseStringByTemplate("video_token:\\W'([a-zA-Z0-9]+)'", webcontent)));
+		list.add(new BasicNameValuePair("content_type", parseStringByTemplate("content_type:\\W'([a-zA-Z0-9]+)'", webcontent)));
+		list.add(new BasicNameValuePair("mw_key", parseStringByTemplate("mw_key:\\W'([a-zA-Z0-9]+)'", webcontent)));
+		list.add(new BasicNameValuePair("mw_pid", parseStringByTemplate("mw_pid:\\W([a-zA-Z0-9]+)", webcontent)));
+		list.add(new BasicNameValuePair("p_domain_id", parseStringByTemplate("p_domain_id:\\W([a-zA-Z0-9]+)", webcontent)));
+		list.add(new BasicNameValuePair("debug", parseStringByTemplate("debug:\\W([a-zA-Z0-9]+)", webcontent)));
+		list.add(new BasicNameValuePair("condition_safe", parseStringByTemplate("condition_safe\\W=\\W'([a-zA-Z0-9]+)", webcontent)));
+		list.add(new BasicNameValuePair("ad_attr:", "0"));
+		return list;
+	}
+	private String parseStringByTemplate(String template, String content) {
+		Pattern regex = Pattern.compile(template);
+		Matcher m = regex.matcher(content);
+		if(m.find()) {
+			System.out.println(m.group(1));
+			return m.group(1);
+		}
+		return "";
 	}
 	private Map<String, String> getHlsStreamMap(String link) {
 		Map<String, String> videoMap = new LinkedHashMap<>();
