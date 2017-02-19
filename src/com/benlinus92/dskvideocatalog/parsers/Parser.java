32e6b0615cfcb0c7f83a081793ebc85a40adda5c
father-gregor <benlinus92@gmail.com>
2017-02-19 19:14:11 +0200

    Change interface to abstract class

--- src/com/benlinus92/dskvideocatalog/parsers/Parser.java
+++ src/com/benlinus92/dskvideocatalog/parsers/Parser.java
@@ -5,6 +5,9 @@ import java.util.List;
 import java.util.Map;
 
 import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.impl.client.HttpClientBuilder;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 
@@ -13,14 +16,29 @@ import com.benlinus92.dskvideocatalog.model.MediaStream;
 import com.benlinus92.dskvideocatalog.model.SimpleVideoItem;
 import com.benlinus92.dskvideocatalog.model.VideoLink;
 
-public interface Parser {
-	public String getHtmlContent(String url) throws IOException;
-	public List<SimpleVideoItem> getVideoItemsByCategory(int category, int page);
-	public BrowserVideoItem getVideoItemByUrl(String url);
-	public SimpleVideoItem createCatalogVideoItemFromHtml(Element el);
-	public BrowserVideoItem createBrowserVideoItemFromHtml(Element el);
-	public Map<String, String> getVideoStreamMap(VideoLink video, MediaStream type);
-	public List<MediaStream> getMediaStreamsList();
-	public Map<String, Integer> getParserCategoryMap();
-	public String getWebSiteName();
+public abstract class Parser {
+	protected Map<String, Integer> parserCategoryMap;
+	protected List<MediaStream> mediaStreamsList;
+	public abstract String getHtmlContent(String url) throws IOException;
+	public abstract List<SimpleVideoItem> getVideoItemsByCategory(int category, int page);
+	public abstract BrowserVideoItem getVideoItemByUrl(String url);
+	public abstract SimpleVideoItem createCatalogVideoItemFromHtml(Element el);
+	public abstract BrowserVideoItem createBrowserVideoItemFromHtml(Element el);
+	public abstract Map<String, String> getVideoStreamMap(VideoLink video, MediaStream type);
+	public abstract String getWebSiteName();
+	
+	protected HttpClient getHttpClient(int timeout) {
+		return HttpClientBuilder.create().setDefaultRequestConfig(
+				RequestConfig.custom()
+				.setConnectTimeout(1000 * timeout)
+				.setConnectionRequestTimeout(1000 * timeout).build())
+				.build();
+	}
+	public List<MediaStream> getMediaStreamsList() {
+		return mediaStreamsList;
+	}
+	
+	public Map<String, Integer> getParserCategoryMap() {
+		return parserCategoryMap;
+	}
 }
